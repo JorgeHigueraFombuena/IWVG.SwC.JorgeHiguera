89bca6db28a155676949613cb791044265daf3fc
juan <juan@dani.dinamiq.local>
2016-09-29 10:29:11 +0200

    method minor

--- src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
@@ -39,5 +39,10 @@ public class DecimalCollectionTest {
     public void testHigher() {
         assertEquals(3, this.dc.higher(), 10e-5);
     }
+    
+    @Test
+    public void testMinor() {
+        assertEquals(-1, this.dc.minor(), 10e-5);
+    }
 
 }
